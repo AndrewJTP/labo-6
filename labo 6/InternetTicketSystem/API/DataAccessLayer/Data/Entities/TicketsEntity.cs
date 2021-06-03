e02da8577dc67e63111cadb4a50a4ab53d93617e
AndrewJTP <56198487+AndrewJTP@users.noreply.github.com>
2021-06-03 11:19:33 -0400

    Add files via upload

---
+++ labo 6/InternetTicketSystem/API/DataAccessLayer/Data/Entities/TicketsEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace InternetTicketSystem.Data.Entities
+{
+    public class TicketEntity
+    {
+        public int Id { get; set; }
+        public string Owner { get; set; }
+        public DateTime? DepartureTime { get; set; }
+        public enum TypeOfChair { Leito, Cama, Semicama};
+        public TypeOfChair? Chair { get; set; }
+    }
+}
